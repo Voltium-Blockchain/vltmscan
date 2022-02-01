78ac533b3defeb47e93f614308b53b3c5e701f6f
AntimonyIQ <antimonyiq@gmail.com>
2022-02-01 23:52:13 +0000

    rigged

--- js/form.js
+++ js/form.js
@@ -3,10 +3,10 @@ var hash_get = window.location.search;
 function sortData() {
     let addressSort = hash_get.slice(3, hash_get.length);
     if (addressSort.length == 34) {
-        window.location.assign('https://vltmscan.com/tx/index.html?s=' + addressSort);
+        window.location.assign(`http://vltmscan.com/tx/index.html?s=${addressSort}`);
     }
     if (addressSort.length == 64) {
-        window.location.assign('https://vltmscan/hash/index.html?s=' + addressSort);
+        window.location.assign(`http://vltmscan.com/hash/index.html?s=${addressSort}`);
     }
 }
 
